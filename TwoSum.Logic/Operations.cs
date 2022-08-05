de886a750d31ecb561a4dce2c758cd3476c3bcef
maksym.tsikalenko <maksym.tsikalenko@pm.bet>
2022-08-05 13:00:12 +0300

    comment

---
+++ TwoSum.Logic/Operations.cs
@@ -0,0 +1,20 @@
+﻿namespace TwoSum.Logic;
+
+public class Operations
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new []{i,j};
+                }
+            }
+        }
+
+        throw new Exception( "blabla");
+    }
+}
