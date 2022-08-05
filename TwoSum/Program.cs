35bdf8a3c73a8ff3fce521e7bd55209a13082590
maksym.tsikalenko <maksym.tsikalenko@pm.bet>
2022-08-05 14:38:22 +0300

    Revert "Revert "comment""
    This reverts commit 72afce776409e882a6ef75a38bdfff6d769664d9.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using TwoSum.Logic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var twoSum = new TwoSum.Logic.Operations();
+        var numbers = new[] { 2, 7, 11, 15};
+        var result  = twoSum.TwoSum(numbers, 9);
+        Console.WriteLine($"{result[0]} {result[1]}");
+    }
+}
